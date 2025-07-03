059083b2d226b26ceff39a48ba87d9794ea6022b
swapnil <swapnilghayal007@gmail.com>
2025-07-03 11:05:10 +0530

    loops

--- demo.java
+++ demo.java
@@ -1,5 +1,13 @@
 public class demo {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        for (int i = 0; i < 6; i++) {
+            System.out.println("Hello World");
+            System.out.println("Hello Swapnil");
+
+        }
+        // System.out.println("Hello World");
+
     }
+
 }
